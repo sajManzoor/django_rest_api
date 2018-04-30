787a2ac6dc702f8d8362249617ad2d0be3a6bfd0
DSONE\sko3 <sajidmanzoor55@gmail.com>
2018-04-30 14:37:26 +0800

    added search functionality

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -7,6 +7,7 @@ from rest_framework import viewsets
 from . import models
 from . import permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 
 # Create your views here.
@@ -112,5 +113,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
-    
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
+
+
 
